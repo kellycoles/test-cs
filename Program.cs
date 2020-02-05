47e7a75ebf95f38ffa98dc195fc316929a1e665a
kellycoles <kellycoles1@gmail.com>
2020-02-05 13:38:18 -0600

    string reverse

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 namespace test
 {
+   
+
     class Program
     {
         static void Main(string[] args)
@@ -31,38 +33,51 @@ namespace test
             // value parameter
             //int i = 0;
             //MyMethod(i);
-            //Console.WriteLine(i);
+            //Console.WriteLine("i is: {0}", i);
 
             // reference parameter
+            //===================
             //int i = 0;
             //MyMethod(ref i);
-            //Console.WriteLine(i);
+            //Console.WriteLine("i is: {0}", i);
 
-            //out parameters
+            //out parameter
+            //================
             //int total = 0;
             //int product = 0;
             //Calculate(10, 20, out total, out product);
             //Console.WriteLine("the total is {0} and the product is {1}", total,product);
 
             //params parameters
-            int[] Numbers = new int[3];
-            Numbers[0] = 101;
-            Numbers[1] = 344;
-            Numbers[2] = 21;
-            ParamsMethod(14, 2, 11);
+            //==================
+            //int[] Numbers = new int[3];
+            //Numbers[0] = 101;
+            //Numbers[1] = 344;
+            //Numbers[2] = 21;
+
             //ParamsMethod(Numbers);
+
+            //this works, too
+            //ParamsMethod(14, 2, 11);
+
+
+            // reverse string line 99
+            string result =ReverseStringDirect(" The dog went for a walk");
+            Console.WriteLine(result);
         }
-       
+
         // value parameter method
         //public static void MyMethod(int j)
         //{
         //    j = 101;
+        //    Console.WriteLine("j is: {0}", j);
         //}
 
-        //// reference parameter method
+        // reference parameter method
         //public static void MyMethod(ref int j)
         //{
         //    j = 101;
+        //Console.WriteLine("j is: {0}", j);
         //}
 
         //out parameter method
@@ -74,13 +89,26 @@ namespace test
 
         // params parameter method
         //params keyword makes passing parameters optional...can be only one number
-        public static void ParamsMethod(params int[] Numbers)
+        //public static void ParamsMethod(params int[] Numbers)
+        //{
+        //    Console.WriteLine("There are {0} elements", Numbers.Length);
+        // foreach(int i in Numbers)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+        // reverse string
+        public static string ReverseStringDirect(string s)
         {
-            Console.WriteLine("There are {0} elements", Numbers.Length);
-         foreach(int i in Numbers)
+            char[] array = new char[s.Length];
+            int forward = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
             {
-                Console.WriteLine(i);
+                array[forward++] = s[i];
             }
+            return new string(array);
         }
+
     }
 }
